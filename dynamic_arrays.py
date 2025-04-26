1912508a0d40535b4284e9b63741f795912f683a
bhomik005 <bhomik005@gmail.com>
2025-04-26 22:54:47 +1000

    updated dynamic arrays implementation

--- dynamic_arrays.py
+++ dynamic_arrays.py
@@ -7,14 +7,17 @@ class DynamicArray:
         self.capacity = capacity
 
     # fetch the element at index i (assuming i is a valid index)
+    # O(1) time
     def get(self, i: int) -> int:
         return self.arr[i]
 
     # overwrite the value at index i to n (assuming i is a valid index)
+    # O(1) time
     def set(self, i: int, n: int) -> None:
         self.arr[i] = n
 
     # push the element n to the end of array
+    # Amortised time - O(1) - most of the operations are constant time but some of them are linear
     def pushback(self, n: int) -> None:
         # check if size is equal to capacity, then we have no space
         # to accomodate any element
@@ -26,12 +29,14 @@ class DynamicArray:
         self.size += 1        
 
     # pop and return the last element
+    # O(1) time
     def popback(self) -> int:
         # decrement the size
         self.size -= 1
         # return the last element
         return self.arr[self.size]
 
+    # O(n) time
     def resize(self) -> None:
         # double the capacity
         self.capacity = self.capacity * 2
@@ -44,9 +49,11 @@ class DynamicArray:
         self.arr = newArr
 
     # return the number of elements in the array
+    # O(1) time
     def getSize(self) -> int:
         return self.size
 
     # return the capacity of the array
+    # O(1) time
     def getCapacity(self) -> int:
         return self.capacity
