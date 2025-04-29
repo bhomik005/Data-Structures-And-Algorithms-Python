8a8e06e2239f85830e89d7788f472722c5442766
bhomik005 <bhomik005@gmail.com>
2025-04-29 23:47:50 +1000

    stack data structure implementation

---
+++ stack.py
@@ -0,0 +1,13 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, n):
+        self.stack.append(n)
+    
+    def pop(self):
+        self.stack.pop()
+
+    def peak(self):
+        return self.stack[len(self.stack) - 1]
+
